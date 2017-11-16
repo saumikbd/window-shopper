31c68ec31210cd1afd809108759f66c7af020b98
Sadman Sakib Saumik <>
2017-11-17 01:39:01 +0600

    Input Accesory View Calculate Button Added

--- Window Shopper/Controller/MainVC.swift
+++ Window Shopper/Controller/MainVC.swift
@@ -9,12 +9,23 @@
 import UIKit
 
 class MainVC: UIViewController {
-
+    @IBOutlet weak var wageTxt: CustomTextField!
+    @IBOutlet weak var priceTxt: CustomTextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        let calcBtn = UIButton()
+        calcBtn.frame = CGRect(x: 0, y: 0, width: view.frame.size.width, height: 60)
+        calcBtn.backgroundColor = #colorLiteral(red: 0.9155091643, green: 0.6786740422, blue: 0.05688857287, alpha: 1)
+        calcBtn.setTitle("Calculate", for: .normal)
+        calcBtn.setTitleColor(#colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0), for: .normal)
+        calcBtn.addTarget(self, action: #selector(claculation), for: .touchUpInside)
+        wageTxt.inputAccessoryView = calcBtn
+        priceTxt.inputAccessoryView = calcBtn
     }
-
+    @objc func claculation(){
+        print("claculation tapped")
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
--- Window Shopper/View/CustomTextField.swift
+++ Window Shopper/View/CustomTextField.swift
@@ -8,8 +8,27 @@
 
 import UIKit
 
+@IBDesignable
 class CustomTextField: UITextField {
 
+    override func prepareForInterfaceBuilder() {
+        super.prepareForInterfaceBuilder()
+        TheDesign()
+    }
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        TheDesign()
+    }
+    func TheDesign(){
+        backgroundColor = #colorLiteral(red: 0.9999960065, green: 1, blue: 1, alpha: 0.25)
+        layer.cornerRadius = 5
+        textAlignment = .center
+        if let place = placeholder{
+            attributedPlaceholder=NSAttributedString(string: place, attributes: [.foregroundColor: #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0) ])
+        }
+        textColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+    }
     /*
     // Only override draw() if you perform custom drawing.
     // An empty implementation adversely affects performance during animation.
