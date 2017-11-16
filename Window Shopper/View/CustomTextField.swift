acf94cfd7bc6594384597da59bca370206effe60
Sadman Sakib Saumik <>
2017-11-16 20:10:23 +0600

    assets added

---
+++ Window Shopper/View/CustomTextField.swift
@@ -0,0 +1,21 @@
+//
+//  CustomTextField.swift
+//  Window Shopper
+//
+//  Created by Sadman Sakib Saumik on 11/16/17.
+//  Copyright © 2017 Sadman Sakib Saumik. All rights reserved.
+//
+
+import UIKit
+
+class CustomTextField: UITextField {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
